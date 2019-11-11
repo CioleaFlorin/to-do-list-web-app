f594a7e1f608558a5d01ea0599f89df99a1d46e1
Florin <ciolea.florin.99@gmail.com>
2019-11-11 21:29:44 +0200

    added suport for updating and deleting items

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -34,7 +34,32 @@ window.ToDoList = {
 
     },
 
+    markItemDone: function(id, done){
+        let requestBody={
+            done:done
+        };
 
+        $.ajax({
+          url:ToDoList.API_URL + "?id="+id,
+          method:"PUT",
+          contentType: "application/json",
+          data: JSON.stringify(requestBody)
+        }).done(function () {
+            ToDoList.getItems();
+
+        })
+    },
+
+    deleteItem: function(id){
+        $.ajax({
+            url:ToDoList.API_URL + "?id="+id,
+            method:"DELETE",
+
+        }).done(function () {
+            ToDoList.getItems();
+
+        })
+    },
 
 
 
@@ -65,6 +90,27 @@ window.ToDoList = {
 
             event.preventDefault();
             ToDoList.createItem();
+
+        });
+
+        //delegate is necessary because our checkbox is dynamically injected in the page
+        //not present from the beginning, on page load
+
+        $("#to-do-items").delegate(".mark-done","change", function (event) {
+            event.preventDefault();
+
+            let id = $(this).data("id");
+            let checked = $(this).is(":checked");
+            ToDoList.markItemDone(id,checked);
+
+
+        });
+
+        $("#to-do-items").delegate(".delete-item","click",function (event) {
+            event.preventDefault();
+
+            let id=$(this).data("id");
+            ToDoList.deleteItem(id);
 
         })
 
